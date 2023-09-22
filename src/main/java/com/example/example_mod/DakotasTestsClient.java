6f650b0da9d59ee20454909ef5b61429aa07dc2e
dktmogi <dktmogi@gmail.com>
2023-09-22 00:33:45 -0300

    basic set up
    i think its ok but maybe need change on entrypoints inside quilt.mod.json

---
+++ src/main/java/com/example/example_mod/DakotasTestsClient.java
@@ -0,0 +1,11 @@
+package com.example.example_mod;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class DakotasTestsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+
+	}
+}
